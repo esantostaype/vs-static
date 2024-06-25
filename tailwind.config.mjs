2d958dd38c769fdfa4a2f18ff6c977caa9454f73
Erick <esantostaype@gmail.com>
2024-06-24 19:30:05 -0500

    smooth

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,15 +2,13 @@ import animations from "tailwindcss-animated";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    theme: {
-        extend: {
-            fontFamily: {
-                roboto: ['Roboto', 'sans-serif']
-            }
-        }
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter'],
+      },
     },
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    plugins: [
-        animations
-    ],
-}
+  },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [animations],
+};
